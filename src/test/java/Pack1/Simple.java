06029fce42d2205961e102ce46b46014789ffb9f
janudhalujanu <nagarajujyothi98@gmail.com>
2023-03-10 15:43:08 +0530

    jhjh

---
+++ src/test/java/Pack1/Simple.java
@@ -0,0 +1,12 @@
+package Pack1;
+
+import org.testng.annotations.Test;
+
+public class Simple {
+@Test
+	public static void test1() {
+	System.out.println("hello");
+
+	}
+
+}
---
+++ src/test/java/Pack1/Simple2.java
@@ -0,0 +1,14 @@
+package Pack1;
+
+import org.testng.annotations.Test;
+
+public class Simple2 {
+	@Test
+
+	public static void test2() {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+
+	}
+
+}
